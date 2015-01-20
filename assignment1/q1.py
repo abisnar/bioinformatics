9d8ae237c3c804a64ae527aefa03ce0616a87562
abisnar <allanbisnar5@gmail.com>
2015-01-19 23:47:45 -0800

    CS445 a1 first commit

---
+++ assignment1/q1.py
@@ -0,0 +1,15 @@
+#/usr/bin/env python
+# Allan Bisnar
+from Bio.Seq import Seq
+
+## Compute similarity matrix for RNA based on the following alignment of
+## 2 rRNA sequences
+
+rna1 = "-----CCACCCGGCGAUAGUGAGCGGGCAACACCCGGACUCAUCUCGAACCCGGAAGUAAAG-UCCCCUACGUUGGUAAG-GCA--GUGGGAUCCGCAAGGGCCUGCAGCCUUGCCAAGCUGGGAUGGACAUU"
+rna2 = "GAUGGGUGCACGGUCAUAGCGGUGGAGUU-UACCCGGUCUCAUCCCGAACCCGGAAGUCAAGCCCUCCUGCGUC-UGUCCC-AAUACUGUGGUACGAGAGUCCACGGGAACGGCGGUCACUGUG-C-------"
+
+seq1 = Seq(rna1)
+seq2 = Seq(rna2)
+
+print str(seq1.count("A")) + " " + str(seq1.count("C")) + " " + str(seq1.count("U")) + " " + str(seq1.count("G"))
+print str(seq2.count("A")) + " " + str(seq2.count("C")) + " " + str(seq2.count("U")) + " " + str(seq2.count("G"))
