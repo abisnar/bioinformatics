0ac73e776409e841cc08f81276e0991018f01671
Allan Bisnar <allanbisnar5@gmail.com>
2015-01-22 22:01:46 -0800

    updated random model to concat both strings

--- assignment1/q1.py
+++ assignment1/q1.py
@@ -10,14 +10,20 @@ from math import log10
 rna1 = "-----CCACCCGGCGAUAGUGAGCGGGCAACACCCGGACUCAUCUCGAACCCGGAAGUAAAG-UCCCCUACGUUGGUAAG-GCA--GUGGGAUCCGCAAGGGCCUGCAGCCUUGCCAAGCUGGGAUGGACAUU"
 rna2 = "GAUGGGUGCACGGUCAUAGCGGUGGAGUU-UACCCGGUCUCAUCCCGAACCCGGAAGUCAAGCCCUCCUGCGUC-UGUCCC-AAUACUGUGGUACGAGAGUCCACGGGAACGGCGGUCACUGUG-C-------"
 
+#concat both strings
+random_rna = rna1 + rna2
+
+length_both = len(random_rna)
+
 seq1 = Seq(rna1)
 seq2 = Seq(rna2)
-length_seq1 = len(rna1)
-length_seq2 = len(rna2)
+length_seq1 = len(rna1) - seq1.count("-")
+length_seq2 = len(rna2) - seq2.count("-")
 
 print str(seq1.count("A")) + " " + str(seq1.count("C")) + " " + str(seq1.count("U")) + " " + str(seq1.count("G"))
 print str(seq2.count("A")) + " " + str(seq2.count("C")) + " " + str(seq2.count("U")) + " " + str(seq2.count("G"))
 print length_seq1
+print length_seq2
 
 pa1 = float(seq1.count("A")) / length_seq1
 pa2 = float(seq2.count("A")) / length_seq2
@@ -31,9 +37,19 @@ pu2 = float(seq2.count("U")) / length_seq2
 pg1 = float(seq1.count("G")) / length_seq1
 pg2 = float(seq2.count("G")) / length_seq2
 
-random_model = pa1 * pa2 * pc1 * pc2 * pu1 * pu2 * pg1 * pg2
+pa = float(random_rna.count("A")) / length_both
+pc = float(random_rna.count("C")) / length_both
+pu = float(random_rna.count("U")) / length_both
+pg = float(random_rna.count("G")) / length_both
 
-print random_model
+#random_model = pa1 * pa2 * pc1 * pc2 * pu1 * pu2 * pg1 * pg2
+random_model = pa * pc * pu * pg
+
+print "Pa is:" + str(pa)
+print "Pc is:" + str(pc)
+print "Pu is:" + str(pu)
+print "Pg is:" + str(pg)
+print "Random score is: " + str(random_model)
 
 def similar(a,b):
     return SequenceMatcher(None,a,b).ratio()
