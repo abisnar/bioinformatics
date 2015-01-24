523233e8a3475936d69266681928dd509dfd7134
Allan Bisnar <allanbisnar5@gmail.com>
2015-01-23 22:15:42 -0800

    Print the top 3 candidates and cleaned up the Protein Name Headers

--- assignment1/q4/q4.py
+++ assignment1/q4/q4.py
@@ -1,6 +1,6 @@
 #!/bin/bash/env python
 from scripts import *
-
+import re
 
 def global_alignment_affinity_gap(n, m, scoring_matrix, d, e):
     # Initialize a 3-D Matrix which Stores the 3 separate Matrices Mij, Gx, Gy
@@ -32,30 +32,47 @@ def global_alignment_affinity_gap(n, m, scoring_matrix, d, e):
     # Get the maximum score
     matrix_scores = [S[0][i][j], S[1][i][j], S[2][i][j]]
     max_score = max(matrix_scores)
-    return str(max_score)
+    print "The alignment score is : " + str(max_score)
+    return max_score
 
 def main():
-    #Gap Penalty
+    # Gap Penalty
     delta = 8
     
     # Affine Penalty
     eps = 3
-    
+
+    # List of all 1017 Uniprot Proteins in Tuples by (id,seq)
     proteins = read_fasta("input/uniprot-organism.fasta")
-    
+
     sequences = get_seqs_from_records(proteins)
     ids = get_seq_id_from_records(proteins)
-    
+
+    # Access the unknown Protein
     with open('input/unknown.txt') as unknown_seq:
-         i =unknown_seq.read().strip()
-    
-    result_scores = [global_alignment_affinity_gap(i,j,BLOSUM62(),delta,eps) for j in sequences]
-    result_list = zip(ids,result_scores)
+        i = unknown_seq.read().strip()
+
+    # Calculating the Global Alignment score of the unknown to all UniProt Proteins
+    result_scores = [global_alignment_affinity_gap(i, j, BLOSUM62(),delta,eps) for j in sequences]
+
+    # Make a new list that contains the ID with Corresponding Alignment Score
+    result_list = zip(ids, result_scores)
+
+    # Sort the list by decreasing Alignment Scores
     sorted_by_max_scores = sorted(result_list, key=lambda tup:tup[1])[::-1]
 
-    with open('output/top3_scores.txt', 'w') as output:
-        output.write(str(sorted_by_max_scores[0]) + '\n'+ str(sorted_by_max_scores[1]) + '\n' + str(sorted_by_max_scores[2]))
+    # Store the Top 3 Candidates
+    top3 = sorted_by_max_scores[0:3]
 
+    # Print out the Top 3 Candidates in a txt file in /output/top3_scores.txt
+    with open('output/top3_scores.txt', 'w') as output:
+        output.write("The Top 3 Global Alignment scores for d = 8 and e = 3 are:" + "\n")
+        for elem in top3:
+            # REGEX pattern to clean up FASTA ID to return only the Protein Name
+            filtered_name = re.search(r'(\d?[A-Z])*\w*[_](\d?[A-Z])*', elem[0])
+            output.write('\n' + 'Name: ' + filtered_name.group() +' , Score: ' + str(elem[1]))
+
+# Commented out block was to test q5
    # delta = 4
    # epsilon = 1
    # i = 'XX'
