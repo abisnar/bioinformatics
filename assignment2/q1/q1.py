2ddcde169a13fe34df75c5b02b92efa3dc0a5957
Allan Bisnar <allanbisnar5@gmail.com>
2015-02-09 13:03:28 -0800

    fixed log rule in q1, fixed q4 computation output

--- assignment2/q1/q1.py
+++ assignment2/q1/q1.py
@@ -12,9 +12,22 @@ def local_alignment_linear_gap(v, w, scoring_matrix, d):
     # Fill in the scores for the lower, middle, upper, and backtrack matrices.
     for i in xrange(1, len(v)+1):
         for j in xrange(1, len(w)+1):
-            scores = [S[i-1][j] - d, S[i][j-1] - d, S[i-1][j-1] + scoring_matrix[v[i-1], w[j-1]], 0]
+            row_scores = S[i-1][j] - d
+            col_scores = S[i][j-1] - d
+            dia_scores = S[i-1][j-1] + scoring_matrix[v[i-1], w[j-1]]
+
+            scores = [0, row_scores, col_scores, dia_scores]
             S[i][j] = max(scores)
-            backtrack[i][j] = scores.index(S[i][j]) ## what if multiple 0 values?
+
+            ## 0 scores denoted by 0, row scores = 1, col scores, and dia scores = 3
+            if S[i][j] == 0:
+                backtrack[i][j] = 0
+            elif S[i][j] == row_scores:
+                backtrack[i][j] = 1
+            elif S[i][j] == col_scores:
+                backtrack[i][j] = 2
+            else:
+                backtrack[i][j] = 3
 
    # Initialize the values of i, j and the aligned sequences.
     i,j = len(v), len(w)
@@ -22,23 +35,24 @@ def local_alignment_linear_gap(v, w, scoring_matrix, d):
 
     max_score = max([S[i][j]])
 
-    # Quick lambda function to insert indels.
+    # Lambda function to insert indels.
     insert_indel = lambda word, i: word[:i] + '-' + word[i:]
 
     # Backtrack to the edge of the matrix starting bottom right.
     while i*j != 0:
-        if backtrack[i-1][j] == 1:
+        if backtrack[i][j] == 1:
             i -= 1
             w_aligned = insert_indel(w_aligned, j)
 
-        if backtrack[i][j-1] == 1:
+        if backtrack[i][j-1] == 2:
             j -= 1
             v_aligned = insert_indel(v_aligned, i)
 
-        else:
-            if backtrack[i-1][j-1] == 0:
+        if backtrack[i-1][j-1] == 3:
                 i -= 1
                 j -= 1
+        else:
+            break
 
 
     # Prepend the necessary preceeding indels to get to (0,0).
