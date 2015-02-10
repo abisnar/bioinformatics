20c60ae086578c0d9df92475a9b2871c500cf642
Allan Bisnar <allanbisnar5@gmail.com>
2015-02-09 19:21:15 -0800

    implemented top 3

--- assignment2/q1/q1.py
+++ assignment2/q1/q1.py
@@ -1,15 +1,15 @@
 #!/bin/bash/env python
 # Allan Bisnar
 from scripts import *
-import re
+
 
 def local_alignment_linear_gap(v, w, scoring_matrix, d):
     # Returns the local alignment score of v and w with constant gap penalty
     # Initialize the matrices.
     S = [[0 for j in xrange(len(w)+1)] for i in xrange(len(v)+1)]
-    backtrack = [[0 for j in xrange(len(w)+1)] for i in xrange(len(v)+1)]
-    max_i_j = 0
-    max_at_i_j = (0,0)
+    backtrack = [["-" for j in xrange(len(w)+1)] for i in xrange(len(v)+1)]
+    max_val = 0
+    max_ij = (0, 0)
 
     # Fill in the scores for the lower, middle, upper, and backtrack matrices.
     for i in xrange(1, len(v)+1):
@@ -20,66 +20,111 @@ def local_alignment_linear_gap(v, w, scoring_matrix, d):
 
             scores = [0, row_scores, col_scores, dia_scores]
             S[i][j] = max(scores)
-            if S[i][j] > max_i_j:
-                max_i_j = S[i][j]
-                max_at_i_j = (i,j)
+            if S[i][j] > max_val:
+                max_val = S[i][j]
+                max_ij = (i, j)
 
-            ## 0 scores denoted by 0, row scores = 1, col scores, and dia scores = 3
-            if S[i][j] == 0:
-                backtrack[i][j] = 0
+            ## 0 scores denoted by gapX, gapY, match, zero
+            if S[i][j] == dia_scores:
+                backtrack[i][j] = "match"
             elif S[i][j] == row_scores:
-                backtrack[i][j] = 1
+                backtrack[i][j] = "gapY"
             elif S[i][j] == col_scores:
-                backtrack[i][j] = 2
+                backtrack[i][j] = "gapX"
             else:
-                backtrack[i][j] = 3
+                backtrack[i][j] = "zero"
 
-   # Initialize the values of i, j and the aligned sequences.
-    i,j = max_at_i_j[0], max_at_i_j[1]
+   # Termination
+    i = max_ij[0]
+    j = max_ij[1]
 
+
+    # Backtrack to the edge of the matrix starting from the max val
     v_aligned = ''
     w_aligned = ''
 
-    max_score = max([S[i][j]])
-
-
-    # Backtrack to the edge of the matrix starting bottom right.
     while i*j != 0:
 
-        if backtrack[i][j] == 1:
-            v_aligned += str(v[i])
+        if backtrack[i][j] == "gapY":
+            i -= 1
+            v_aligned += v[i]
             w_aligned += '-'
-            i -= 1
 
-        if backtrack[i][j] == 2:
-            v_aligned += '-'
-            w_aligned += str(w[j])
-
+        elif backtrack[i][j] == "gapX":
             j -= 1
+            v_aligned += '-'
+            w_aligned += w[j]
 
-        if backtrack[i][j] == 3:
+        elif backtrack[i][j] == "match":
+            i -= 1
+            j -= 1
             v_aligned += str(v[i])
             w_aligned += str(w[j])
-            i -= 1
-            j -= 1
 
         else:
             break
 
-    return str(max_score), v_aligned, w_aligned
+    #Reverse the reconstructed local alignments
+    v_aligned = v_aligned[::-1]
+    w_aligned = w_aligned[::-1]
+
+    s_start = i + 1
+    t_start = j + 1
+
+    alignment = {"length": max(len(v_aligned), len(w_aligned)),
+                 "ref seq": v_aligned[0:60],
+                 "ref start": s_start,
+                 "unknown seq": w_aligned[0:60],
+                 "unknown start": t_start}
+
+    print alignment
+    return (max_val, alignment)
 
 def main():
     '''Main call. Reads, runs, and saves problem specific data.'''
     # Parse the two input protein strings.
-    s = 'MRKPAAGFLPSLLKVLLLPLAPAAAQDSTQASTPGSPLSPTEYERFFALLTPTWKAETTCRLRATHGCRNPTLVQLDQYENHGLVPDGAVCSNLPYASWFESFCQFTHYRCSNHVYYAKRVLCSQPVSILSPNTLKEIEASAEVSPTTMTSPISPHFTVTERQTFQPWPERLSNNVEELLQSSLSLGGQEQAPEHKQEQGVEHRQEPTQEHKQEEGQKQEEQEEEQEEEGKQEEGQGTKEGREAVSQLQTDSEPKFHSESLSSNPSSFAPRVREVESTPMIMENIQELIRSAQEIDEMNEIYDENSYWRNQNPGSLLQLPHTEALLVLCYSIVENTCIITPTAKAWKYMEEEILGFGKSVCDSLGRRHMSTCALCDFCSLKLEQCHSEASLQRQQCDTSHKTPFVSPLLASQSLSIGNQVGSPESGRFYGLDLYGGLHMDFWCARLATKGCEDVRVSGWLQTEFLSFQDGDFPTKICDTDYIQYPNYCSFKSQQCLMRNRNRKVSRMRCLQNETYSALSPGKSEDVVLRWSQEFSTLTLGQFG'
-    t = 'MPGLGRRAQWLCWWWGLLCSCCGPPPLRPPLPAAAAAAAGGQLLGDGGSPGRTEQPPPSPQSSSGFLYRRLKTQEKREMQKEILSVLGLPHRPRPLHGLQQPQPPALRQQEEQQQQQQLPRGEPPPGRLKSAPLFMLDLYNALSADNDEDGASEGERQQSWPHEAASSSQRRQPPPGAAHPLNRKSLLAPGSGSGGASPLTSAQDSAFLNDADMVMSFVNLVEYDKEFSPRQRHHKEFKFNLSQIPEGEVVTAAEFRIYKDCVMGSFKNQTFLISIYQVLQEHQHRDSDLFLLDTRVVWASEEGWLEFDITATSNLWVVTPQHNMGLQLSVVTRDGVHVHPRAAGLVGRDGPYDKQPFMVAFFKVSEVHVRTTRSASSRRRQQSRNRSTQSQDVARVSSASDYNSSELKTACRKHELYVSFQDLGWQDWIIAPKGYAANYCDGECSFPLNAHMNATNHAIVQTLVHLMNPEYVPKPCCAPTKLNAISVLYFDDNSNVILKKYRNMVVRACGCH'
-    # Get the alignment score.
-    score = local_alignment_linear_gap(s, t, BLOSUM62(), 4)
+    proteins = read_fasta("input/uniprot-organism.fasta")
+    ref_protein = proteins[999]
+    ref_id = ref_protein[0]
+    seq1 = ref_protein[1]
 
-    # Print and save the answer.
-    print '\n'.join(score)
-    with open('output/q5.txt', 'w') as output_data:
-        output_data.write('\n'.join(score))
+    unknown_proteins = proteins[0:998]
+    unknown_seq = get_seqs_from_records(unknown_proteins)
+    unknown_id = get_seq_id_from_records(unknown_proteins)
+    index = list(xrange(998))
+    id_index = zip(index, unknown_id)
+
+    result_scores = [local_alignment_linear_gap(seq1, seq2, BLOSUM62(), 4) for seq2 in unknown_seq]
+
+    all_results = zip(id_index, result_scores)
+    #sort by max_scores
+    sorted_by_max_scores = sorted(all_results, key=lambda tup: tup[1][0])[::-1]
+
+    top3 = sorted_by_max_scores[0:3]
+
+    print(top3)
+
+    #print top 3 alignment information
+
+    for alignment in top3:
+        print "\n"
+        print "Index= "+str(alignment[0][0])+" Name= "+alignment[0][1]+" Score= "+str(alignment[1][0])
+        print "START POS in "+ref_id+": "+str(alignment[1][1]['ref start'])+ " START POS in "+alignment[0][1]+": "+ str(alignment[1][1]['unknown start'])
+        print alignment[1][1]["ref seq"]
+        print alignment[1][1]["unknown seq"]
+        print "\n"
+
+    # Print out the Top 3 Candidates in a txt file in /output/top3_scores.txt
+    path_to_output = 'output/top3_results.txt'
+    print "printing top 3 to: "+path_to_output
+    with open(path_to_output, 'w') as output:
+        for alignment in top3:
+            output.writelines("\n")
+            output.writelines("Index= "+str(alignment[0][0])+" Name= "+alignment[0][1]+" Score= "+str(alignment[1][0]) +"\n")
+            output.writelines("START POS in "+ref_id+": "+str(alignment[1][1]['ref start'])+ " START POS in "+alignment[0][1]+": "+ str(alignment[1][1]['unknown start']) +"\n")
+            output.writelines(alignment[1][1]["ref seq"] + "\n")
+            output.writelines(alignment[1][1]["unknown seq"] +"\n")
+            output.writelines("\n")
 
 if __name__ == '__main__':
     main()
--- assignment2/q1/scripts/fasta_parser.py
+++ assignment2/q1/scripts/fasta_parser.py
@@ -1,10 +1,11 @@
 from Bio import SeqIO
+import re
 
 def read_fasta(fasta):
     handle = open(fasta)
     records = list(SeqIO.parse(handle,"fasta"))
     handle.close()
-    return[(rec.name, str(rec.seq)) for rec in records]
+    return[(re.sub("^sp\|[\w\d]*\|", "", str(rec.name)), str(rec.seq)) for rec in records]
 
 def get_alignment_seq(fasta):
 	return [fasta[1] for f in fasta]
